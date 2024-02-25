74e1b935a80a6d616d6a4f089cec137b0c12af90
Svillam74495 <104543238+Svillam74495@users.noreply.github.com>
2024-02-25 16:27:57 -0500

    Add files via upload

---
+++ proyectil.java
@@ -0,0 +1,19 @@
+public class proyectil {
+
+	private int velocidad;
+
+	private int angulo;
+
+	public int Proyectil(int int velocidad, int int angulo) {
+		return 0;
+	}
+
+	public int String to String() {
+		return 0;
+	}
+
+	public int void main(int String args) {
+		return 0;
+	}
+
+}
